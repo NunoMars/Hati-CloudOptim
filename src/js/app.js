c8174edeab3b55bd6414f3a18dd2a5146dbad150
NunoMars <nuno.ricardo.mars@gmail.com>
2023-10-02 09:35:12 +0200

    adding form tu add new servers

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,66 @@
 import '../scss/app.scss';
-// Sélectionnez les boutons "Favoris" et ajoutez des gestionnaires d'événements
+// Fonction pour gérer le clic sur le bouton "Favoris"
+function toggleFavorite() {
+    // Trouvez le serveur parent de ce bouton
+    const server = this.closest('.server');
+
+    // Vérifiez si le serveur est favori ou non
+    const isFavorite = server.classList.contains('favorite');
+
+    // Inversez l'état "favori"
+    if (!isFavorite) {
+        server.classList.add('favorite');
+        this.classList.add('favorited');
+        alert('Serveur ajouté aux favoris!');
+    } else {
+        server.classList.remove('favorite');
+        this.classList.remove('favorited');
+        alert('Serveur supprimé des favoris!');
+    }
+}
+
+// Sélectionnez les boutons "Favoris" existants et ajoutez des gestionnaires d'événements
 const favButtons = document.querySelectorAll('.fav-button');
 
 favButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        // Marquez le serveur comme favori
-        const server = button.closest('.server');
-        server.classList.toggle('favorite');
+    button.addEventListener('click', toggleFavorite);
+});
 
-        // Changez la couleur du bouton
-        button.classList.toggle('favorited');
+// Sélectionnez le formulaire d'ajout de serveur
+const addServerForm = document.getElementById('add-server-form');
 
-        // Affichez une notification
-        if (server.classList.contains('favorite')) {
-            alert('Serveur ajouté aux favoris!');
-        } else {
-            alert('Serveur supprimé des favoris!');
-        }
-    });
+// Gestionnaire d'événements pour le formulaire
+addServerForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Empêchez le formulaire de se soumettre normalement
+
+    // Récupérez les valeurs des champs du formulaire
+    const serverName = document.getElementById('server-name').value;
+    const serverStatus = document.getElementById('server-status').value;
+    const serverCPU = document.getElementById('server-cpu').value;
+    const serverMemory = document.getElementById('server-memory').value;
+
+    // Créez un nouvel élément de serveur avec les valeurs du formulaire
+    const newServer = document.createElement('li');
+    newServer.classList.add('server');
+    newServer.innerHTML = `
+        <span class="server-name">${serverName}</span>
+        <span class="server-status">Status: ${serverStatus}</span>
+        <span class="server-cpu">CPU: ${serverCPU}%</span>
+        <span class="server-memory">Memory: ${serverMemory}%</span>
+        <button class="fav-button">Favoris</button>
+    `;
+
+    // Ajoutez ce nouveau serveur à la liste
+    const serverList = document.querySelector('.server-list');
+    serverList.appendChild(newServer);
+
+    // Réinitialisez le formulaire
+    addServerForm.reset();
+
+    // Ajoutez un gestionnaire d'événements au bouton "Favoris" pour le nouveau serveur
+    const favButton = newServer.querySelector('.fav-button');
+    favButton.addEventListener('click', toggleFavorite);
+
+    // Affichez une notification pour indiquer que le serveur a été ajouté
+    alert('Serveur ajouté avec succès!');
 });
